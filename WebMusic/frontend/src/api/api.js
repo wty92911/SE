4c5abf66c294920fcb7bbfeb6af85c0a957944c0
wty92911 <2066946886@qq.com>
2022-09-22 20:05:30 +0800

    fix indent and update README

--- WebMusic/frontend/src/api/api.js
+++ WebMusic/frontend/src/api/api.js
@@ -3,6 +3,6 @@ import axiosInstance from './index'
 
 const axios = axiosInstance
 
-export const search = (name) => {return axios.post('http://47.94.92.103:80/demo/',{'musicname' : name})}
+export const search = (name) => {return axios.post('http://47.94.92.103:80/demo/',{'musicname' : name})};
 //export const getbooks = () => {return 0}
 //export const addbook  = () => {return 0}
