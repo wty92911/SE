bf82e811a3d0ec93b3d0bbd001d43a81435f0eb0
wty92911 <2066946886@qq.com>
2022-10-04 15:42:43 +0800

    build api

--- WebMusic/api/urls.py
+++ WebMusic/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.search,name = 'search')
+    path('getMusic',views.getMusic,name = 'getMusic'),
 ]
--- WebMusic/api/views.py
+++ WebMusic/api/views.py
@@ -1,22 +1,22 @@
+
 from ast import match_case
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-from .search import geturl
+from .import crawler
 from django.views.decorators.http import require_http_methods
 from django.core import serializers
 import json
-from .models import Mymusic
 # Create your views here.
 ##
 @require_http_methods(["GET","POST"])
-def search(request):
+def getMusic(request):
     print(request.body)
     dt = json.loads(request.body)
     name = dt['musicname']
     res = {}
-    music = []
-    for x in geturl(name):
-        music.append(Mymusic(url = x))
-    res['list'] = json.loads(serializers.serialize('json',music))
+    res['musicUrl'] = crawler.get_Music_url(name)
+    print(get_Music_url('the show'))
     return JsonResponse(res)
+
+#print(get_Music_url('the show'))
