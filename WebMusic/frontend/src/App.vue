0619c713a02dc2d045382f534034c77da3b59858
SueJane <80026679+SueJane@users.noreply.github.com>
2022-10-08 18:08:07 +0800

    complete the Header of Home Page

--- WebMusic/frontend/src/App.vue
+++ WebMusic/frontend/src/App.vue
@@ -1,47 +1,17 @@
 <script setup>
   import HelloWorld from './components/HelloWorld.vue'
-  import TheWelcome from './components/TheWelcome.vue'
-  </script>
+  import PageHeader from './components/PageHeader.vue'
+</script>
   
-  <template>
-    <header>
-      <img alt="WebMusic logo" class="logo" src="./assets/logo.png" />
-      
-      
-    </header>
-    <main><HelloWorld msg="Find Music" /></main>
+<template>
+  <div>
+    <PageHeader />
     
-      
-    
-  </template>
+  </div>
+</template>
   
-  <style scoped>
-  header {
-    line-height: 1.5;
-  }
-  
-  .logo {
-    display: block;
-    margin: 0 auto 2rem;
-    width:50%;
-  }
-  
+  <style scoped>   
   @media (min-width: 1024px) {
-    header {
-    line-height: 1.5;
-  }
-  
-    .logo {
-    display: block;
-    margin: 0 auto 2rem;
-    width:50%;
-  }
-  
-    header .wrapper {
-      display: flex;
-      place-items: flex-start;
-      flex-wrap: wrap;
-    }
   }
   </style>
   
--- WebMusic/frontend/src/main.js
+++ WebMusic/frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import ElementUI from 'element-plus'
 import Axios from 'axios'
 import VueAxios from 'vue-axios'
-import './assets/main.css'
+
 const app = createApp(App).use(VueAxios,Axios)
 app.use(ElementUI)
 app.mount('#app')
