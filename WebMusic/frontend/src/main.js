9699a86310e353ca3b80effbc9220e1a105b73e4
SueJane <80026679+SueJane@users.noreply.github.com>
2022-10-29 17:18:04 +0800

    add a playpage

--- WebMusic/frontend/src/main.js
+++ WebMusic/frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import ElementUI from 'element-plus'
 import Axios from 'axios'
 import VueAxios from 'vue-axios'
+import Play from './Play.vue'
 
 const app = createApp(App).use(VueAxios,Axios)
 app.use(ElementUI)
