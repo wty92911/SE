929558c994453d2e0f213678350e4e36604a7ea5
wty92911 <2066946886@qq.com>
2022-10-29 16:49:49 +0800

    add api gethotlist and modify pic in the homepage

--- WebMusic/frontend/src/api/api.js
+++ WebMusic/frontend/src/api/api.js
@@ -5,3 +5,4 @@ const axios = axiosInstance
 
 //export const search = (name) => {return axios.post('http://47.94.92.103:80/demo/',{'musicname' : name})};
 export const searchMusic = (name) => {return axios.post('getMusic',{'musicname':name})};
+export const getHotlist = () => {return axios.post('getHotlist')};
