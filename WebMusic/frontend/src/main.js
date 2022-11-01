36c9b4ac45f08abcfea3896c9fc2e1e34a2f6f11
LYZ-Helper <lyz2947746218@gmail.com>
2022-11-01 22:47:32 +0800

    fix main.js

--- WebMusic/frontend/src/main.js
+++ WebMusic/frontend/src/main.js
@@ -11,7 +11,7 @@ const Player = {template : '<div>Play</div>'}
 const app = createApp(App).use(VueAxios,Axios)
 const routes = [
     {path :"/LikesHots",component: Like},
-    {path :"/Search",component:Search},
+    {path :"/Search",component: Search},
 ]
 const router = createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
