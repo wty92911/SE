e34f0b30c434043d3c5e34fecc9fc7a714443425
SueJane <80026679+SueJane@users.noreply.github.com>
2022-11-04 17:02:36 +0800

    update the hotlist, and add jump to the play page

--- WebMusic/frontend/src/main.js
+++ WebMusic/frontend/src/main.js
@@ -24,6 +24,7 @@ const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(V
 const routes = [
     {path :"/LikesHots",name:"LikesHots",component: Like}, 
     {path :"/Search",name:"Search",component: Search},
+    {path :"/Play",name:"Play",component: Play},
 ]
 const router = createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
