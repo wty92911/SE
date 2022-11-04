939f1763c862bf2577dcda235b1694332639eaa0
wty92911 <2066946886@qq.com>
2022-11-04 17:48:18 +0800

    add signin and signout api and pages

--- WebMusic/frontend/src/Sign_in.vue
+++ WebMusic/frontend/src/Sign_in.vue
@@ -1,30 +1,185 @@
-<script setup>
-import { ref } from 'vue'
-
-const text1 = ref('')
-const text2 = ref('')
-
+<script>
+import {mySignIn,mySignUp} from './api/api.js'
+export default{
+  props:{
+    showSignIn:{
+      type:Boolean,
+      default:true,
+    },
+    userName:{
+      type:String,
+    }
+  },
+  data(){
+    return{
+      usr : '',
+      pwd : '',
+      msg : '',
+    }
+  },
+  methods:{
+    close(){
+      this.$parent.showSignIn = !this.$parent.showSignIn;
+    },
+    signIn(){
+      mySignIn(this.usr,this.pwd).then(
+        (res)=>{
+          this.msg = res.data.message;
+          if(res.data.auth == true){
+            this.$parent.userName = res.data.userName;
+          }else{
+            this.$parent.userName = '未登录';
+          }
+          alert(this.msg)
+          this.close();
+        }
+      )
+    },
+    signUp(){
+      mySignUp(this.usr,this.pwd).then(
+        (res)=>{
+          this.msg = res.data.message;
+          if(res.data.auth == true){
+            this.$parent.userName = res.data.userName;
+          }else{
+            this.$parent.userName = '未登录';
+          }
+          alert(this.msg)
+          this.close();
+        }
+      )
+    }
+  }
+}
 </script>
-
 <template>
-    <div>
-        <img class = "background" src = "./assets/sign_in/矩形 3 Copy@1x.png" />
-        <img class = "logo" src = "./assets/HomePage/logo.png" />
-        <img class = "title" src = "./assets/sign_in/WebMusic@1x.png" />
+  <!-- 整体背景 -->
+  <div class="login-wrap" v-if="showSignIn">
+    <!--输入框-->
+    <div class="form-wrapper">
+      <div class="header">
+        WebMusic
+      </div>
+      <div class="input-wrapper">
+        <div class="border-wrapper">
+          <input v-model="usr" type="text" name="username" placeholder="username" class="border-item" autocomplete="off" />
+        </div>
+        <div class="border-wrapper">
+          <input v-model="pwd" type="password" name="password" placeholder="password" class="border-item" autocomplete="off" />
+        </div>
+      </div>
+      <div class="action">
+        <div class="btn" @click="signIn()">Sign In</div>
+        <div class="btn" @click="signUp()">Sign Up</div>
+      </div>
     </div>
-    <div class = "sign_in_button">
-        <img class = "sign_in_icon" src = "./assets/sign_in/按钮2@1x.png" />
-    </div>
-    <div class = "username">
-        <input v-model="text1" placeholder="username">
-        <p>{{ text1 }}</p>
-    </div>
-    <div>
-        <img 
-    </div>
-    <div class = "password">
-        <input v-model="text2" placeholder="password">
-        <p>{{ text2 }}</p>
-    </div>
+  </div>
 </template>
+ 
+<style scoped>
+.login-wrap {
+  z-index: 101;
+  height: 100%;
+  font-family: JetBrains Mono Medium;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* background-color: #0e92b3; */
+  background-color: rgba(0, 10, 14, 0.58);
+  background-size: 100% 100%;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+ 
+.form-wrapper {
+  width: 300px;
+  background-color: rgba(41, 45, 62, 0.8);
+  color: #fff;
+  border-radius: 10px;
+  padding: 50px;
+}
 
+.form-wrapper .header {
+  color :rgb(127, 223, 255);
+  text-align: center;
+  font-size: 35px;
+  line-height: 100px;
+}
+ 
+.form-wrapper .input-wrapper input {
+  background-color: rgb(41, 45, 62);
+  border: 0;
+  width: 100%;
+  text-align: center;
+  font-size: 15px;
+  color: #fff;
+  outline: none;
+}
+ 
+.form-wrapper .input-wrapper input::placeholder {
+  text-transform: uppercase;
+}
+ 
+.form-wrapper .input-wrapper .border-wrapper {
+  background-image: linear-gradient(to right, #e7d6df, #0eb4dd);
+  width: 100%;
+  height: 50px;
+  margin-bottom: 20px;
+  border-radius: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+ 
+.form-wrapper .input-wrapper .border-wrapper .border-item {
+  height: calc(100% - 4px);
+  width: calc(100% - 4px);
+  border-radius: 30px;
+}
+ 
+.form-wrapper .action {
+  display: flex;
+  justify-content: center;
+}
+ 
+.form-wrapper .action .btn {
+  width: 60%;
+  text-transform: uppercase;
+  border: 2px solid #0e92b3;
+  text-align: center;
+  line-height: 50px;
+  border-radius: 30px;
+  cursor: pointer;
+}
+ 
+.form-wrapper .action .btn:hover {
+  background-image: linear-gradient(120deg, #797e7b 0%, #8fd3f4 100%);
+}
+ 
+.form-wrapper .icon-wrapper {
+  text-align: center;
+  width: 60%;
+  margin: 0 auto;
+  margin-top: 20px;
+  border-top: 1px dashed rgb(146, 146, 146);
+  padding: 20px;
+}
+ 
+.form-wrapper .icon-wrapper i {
+  font-size: 20px;
+  color: rgb(187, 187, 187);
+  cursor: pointer;
+  border: 1px solid #fff;
+  padding: 5px;
+  border-radius: 20px;
+}
+ 
+.form-wrapper .icon-wrapper i:hover {
+  background-color: #0e92b3;
+}
+</style>
--- WebMusic/frontend/src/api/api.js
+++ WebMusic/frontend/src/api/api.js
@@ -6,3 +6,5 @@ const axios = axiosInstance
 //export const search = (name) => {return axios.post('http://47.94.92.103:80/demo/',{'musicname' : name})};
 export const searchMusic = (name) => {return axios.post('getMusic',{'musicname':name})};
 export const getHotlist = () => {return axios.post('getHotlist')};
+export const mySignIn = (username,password) =>{return axios.post('mySignIn',{'username':username,'password':password})};
+export const mySignUp = (username,password) =>{return axios.post('mySignUp',{'username':username,'password':password})};
