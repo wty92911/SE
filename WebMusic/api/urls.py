84bfe64098f5d724cbcfa43d3936fcf341f9c9d7
adromn <2000012978@stu.pku.edu.cn>
2022-11-27 19:29:17 +0800

    弹幕功能

--- WebMusic/api/urls.py
+++ WebMusic/api/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('getMusic',views.getMusic,name = 'getMusic'),
     path('getHotlist',views.getHotlist,name = 'getHotlist'),
     path('mySignIn',views.mySignIn,name='mySignIn'),
-    path('mySignUp',views.mySignUp,name = 'mySignUp')
+    path('mySignUp',views.mySignUp,name = 'mySignUp'),
+    path('getscreens',views.getscreens,name = 'getscreens'),
+    path('likescreens',views.likescreens,name = 'likescreens'),
+    path('dislikescreens',views.dislikescreens,name = 'dislikescreens'),
+    path('printscreens',views.printscreens,name = 'printscreens'),
 ]
--- WebMusic/api/views.py
+++ WebMusic/api/views.py
@@ -1,19 +1,21 @@
-
 from ast import match_case
 from http.client import HTTPResponse
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
-from .import crawler
+from django.http import HttpResponse, JsonResponse
+from . import crawler
 from django.views.decorators.http import require_http_methods
 from django.core import serializers
 from . import models
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate, login
 
 import json
+
 # Create your views here.
 ##
 mp = {}
-@require_http_methods(["GET","POST"])
+
+
+@require_http_methods(["GET", "POST"])
 def getMusic(request):
     print(request.body)
     if request.body in mp:
@@ -23,17 +25,18 @@ def getMusic(request):
     id_list = crawler.get_ID(name)
     res = {}
     res['music'] = [{
-                    'id':id,
-                    'name':crawler.get_Music_name(id),
-                    'artists':crawler.get_artists_name(id),
-                    'url':crawler.get_Music_url(id),
-                    'pic_url':crawler.get_pic_url(id),
-                    } for id in id_list]
+        'id': id,
+        'name': crawler.get_Music_name(id),
+        'artists': crawler.get_artists_name(id),
+        'url': crawler.get_Music_url(id),
+        'pic_url': crawler.get_pic_url(id),
+    } for id in id_list]
     print(res)
     mp[request.body] = JsonResponse(res)
     return JsonResponse(res)
 
-@require_http_methods(["GET","POST"])
+
+@require_http_methods(["GET", "POST"])
 def getHotlist(request):
     print(request.body)
     if request.body in mp:
@@ -41,29 +44,31 @@ def getHotlist(request):
     id_list = crawler.get_Hotlist()
     res = {}
     res['music'] = [{
-                    'id':id,
-                    'name':crawler.get_Music_name(id),
-                    'artists':crawler.get_artists_name(id),
-                    'url':crawler.get_Music_url(id),
-                    'pic_url':crawler.get_pic_url(id),
-                    } for id in id_list]
+        'id': id,
+        'name': crawler.get_Music_name(id),
+        'artists': crawler.get_artists_name(id),
+        'url': crawler.get_Music_url(id),
+        'pic_url': crawler.get_pic_url(id),
+    } for id in id_list]
     print(res['music'])
     mp[request.body] = JsonResponse(res)
     return JsonResponse(res)
-#print(get_Music_url('the show'))
 
-@require_http_methods(["GET","POST"])
+
+# print(get_Music_url('the show'))
+
+@require_http_methods(["GET", "POST"])
 def mySignIn(request):
     print(request.body)
-    
+
     dt = json.loads(request.body)
     user_name = dt['username']
     pass_word = dt['password']
-    res = {}    
+    res = {}
     if user_name and pass_word:
         user_name = user_name.strip()
         try:
-            user = models.User.objects.get(name = user_name)
+            user = models.User.objects.get(name=user_name)
         except:
             res['auth'] = False
             res['message'] = 'error username'
@@ -81,20 +86,22 @@ def mySignIn(request):
     res['auth'] = False
     res['message'] = 'error username or password'
     return JsonResponse(res)
-    
-    #if user is not None:
+
+    # if user is not None:
     #    login(request,user)
-@require_http_methods(["GET","POST"])
+
+
+@require_http_methods(["GET", "POST"])
 def mySignUp(request):
     print(request.body)
     dt = json.loads(request.body)
     user_name = dt['username']
     pass_word = dt['password']
-    res = {}    
+    res = {}
     if user_name and pass_word:
         user_name = user_name.strip()
         try:
-            user = models.User.objects.get(name = user_name)
+            user = models.User.objects.get(name=user_name)
             res['auth'] = False
             res['message'] = 'repeated username'
             return JsonResponse(res)
@@ -110,3 +117,63 @@ def mySignUp(request):
     res['auth'] = False
     res['message'] = 'error username or password'
     return JsonResponse(res)
+
+
+sp = {}
+
+
+@require_http_methods(["GET", "POST"])
+def getscreens(request):
+    print(request.body)
+    dt = json.loads(request.body)
+    music_id = dt['music_id']
+    content = dt['content']
+    source = dt['username']
+    if music_id in sp:
+        sp[music_id] = sp[music_id] + 1
+    else:
+        sp[music_id] = 0
+    scr = {}
+    scr['music_id'] = music_id
+    scr['count'] = sp[music_id]
+    scr['content'] = content
+    scr['source'] = source
+    scr['likes'] = 0
+    scr['dislikes'] = 0
+    obj = models.screens(**scr)
+    obj.save()
+
+
+@require_http_methods(["GET", "POST"])
+def printscreens(request):
+    print(request.body)
+    dt = json.loads(request.body)
+    t_music = dt['music_id']
+    count = sp[t_music]
+    screens = {}
+    for i in range(1, count):
+        obj = models.screens.objects.get(music_id = t_music, screens_count = i)
+        screens[i] = obj
+    return JsonResponse(screens)
+
+
+@require_http_methods(["GET", "POST"])
+def likescreens(request):
+    print(request.body)
+    dt = json.loads(request.body)
+    t_music = dt['music_id']
+    t_screen = dt['screens_count']
+    obj = models.screens.objects.get(music_id = t_music, screens_count = t_screen)
+    obj.likes = obj.likes + 1
+    obj.save()
+
+
+@require_http_methods(["GET", "POST"])
+def dislikescreens(request):
+    print(request.body)
+    dt = json.loads(request.body)
+    t_music = dt['music_id']
+    t_screens = dt['screens_count']
+    obj = models.screens.objects.get(music_id = t_music, screens_count = t_screen)
+    obj.dislikes = obj.dislikes + 1
+    obj.save()
