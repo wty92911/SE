04b7b6e94fa5a30c1b221f512e55a07ccb839091
adromn <2000012978@stu.pku.edu.cn>
2022-11-27 19:29:17 +0800

    弹幕功能

--- WebMusic/api/models.py
+++ WebMusic/api/models.py
@@ -12,4 +12,15 @@ class User(models.Model):
     def __str__(self):
         return self.name
  
-       
+class screens(models.Model):
+    '''弹幕'''
+
+    music_id = models.CharField()
+    screens_count = models.PositiveIntegerField()
+    content = models.CharField(max_length=256)
+    source = models.CharField(max_length=128,unique=True)
+    '''source记录发送者用户名'''
+    likes = models.PositiveIntegerField()
+    dislikes = models.PositiveIntegerField()
+
+
